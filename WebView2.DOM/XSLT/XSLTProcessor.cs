b4bbbbd9149ea98e3e9215ff5a841c3fc4b8b4ca
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2021-09-10 23:44:11 -0500

    placeholder

---
+++ WebView2.DOM/XSLT/XSLTProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebView2.DOM
+{
+	[Obsolete("Not implemented", true)]
+	public class XSLTProcessor
+	{
+		// https://github.com/chromium/chromium/blob/master/third_party/blink/renderer/core/xml/xslt_processor.idl
+	}
+}
